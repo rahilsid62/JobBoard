3a462c8050ab842151d045bcff4dd323749d3106
Rahil Siddiqui <rahilsid62@gmail.com>
2025-06-12 13:51:41 +0530

    updated homelayout

--- src/pages/Homelayout.jsx
+++ src/pages/Homelayout.jsx
@@ -1,8 +1,13 @@
-const HomeLayout = ()=>{
+import {Link} from "react-router-dom";
+
+const HomeLayout = ()=>{ 
     return (
         <div>
             <h1>This Homepage is being opened through the file</h1>
             <h2>Sorry, through the external file</h2>
+            <Link to = '/register'> Register here </Link> <br></br>
+            <Link to = '/login'> Login here </Link> <br></br>
+            <Link to = '/dashboard'> Dashboard here </Link>
         </div>
     )
 }
